3f392e26cf9506948fe766871866d956e18f9a1c
ezhilselvan1109 <ezhilselvan1109@gmail.com>
2025-08-18 13:31:04 +0530

    product type removed

--- src/main/java/com/velsystems/ecommerce/repository/product/ProductTypeRepository.java
+++ src/main/java/com/velsystems/ecommerce/repository/product/ProductTypeRepository.java
@@ -1,6 +1,5 @@
 package com.velsystems.ecommerce.repository.product;
 
-import com.velsystems.ecommerce.model.product.ProductType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
